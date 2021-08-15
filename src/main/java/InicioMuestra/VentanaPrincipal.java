eeb3e2af04b67a56e405b7a929d7208c2b3b3c7d
EdoAbarca <eduardo.abarca.c@usach.cl>
2021-08-15 03:10:55 -0400

    1er commit

---
+++ src/main/java/InicioMuestra/VentanaPrincipal.java
@@ -0,0 +1,32 @@
+
+package InicioMuestra;
+/*
+Laboratorio 4 Paradigmas de Programacio1n
+Objetivo: Implementar un programa que simule las funcionalidades de una red social, usando el Paradigma Orientado a Objetos y Paradigma Orientado a Eventos
+Nombre Alumno: Eduardo Abarca
+Seccion: C-3
+Profesor: Daniel Gacitua
+Entrega: Original (23/08/2021)
+
+VERSION SUJETA A CAMBIOS.
+*/
+
+/**
+ * Clase VentanaPrincipal.
+ * Esta clase tiene por objetivo instanciar la ventana que dara inicio a la muestra de la interfaz grafica.
+ * @version 1.0, 15/08/2021
+ * @author Eduardo Abarca
+ */
+
+public class VentanaPrincipal {
+
+    /** Metodo main
+     * Aca se inicia la ejecucion del programa, instanciando y haciendo visible la ventana principal de la interfaz grafica.
+     * @param args los argumentos ingresados por linea de comandos
+     */
+    public static void main(String[] args)
+    {
+        // En proceso...
+    }
+    
+}
